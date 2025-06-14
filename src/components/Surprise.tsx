4c238055deb2461a66fda2f36af0afdd039f96de
Sumit Sahani <120613334+sumitmsahani@users.noreply.github.com>
2025-06-14 10:39:59 +0530

    Update Surprise.tsx

--- src/components/Surprise.tsx
+++ src/components/Surprise.tsx
@@ -16,8 +16,8 @@ const Surprise = () => {
     "Play Music",
     "Decorate",
     "Fly the Balloons",
-    "Let's Cut the Cake Madam Ji",
-    "Well, I Have a Message for You Madam Ji"
+    "Let's Cut the Cake!!!",
+    "Now, a special little something..."
   ];
 
   const handleClick = () => {
